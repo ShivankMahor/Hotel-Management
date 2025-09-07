4315ca029fe047291623fe89180575442baa68b5
Shivank <shivank88215@gmail.com>
2025-09-07 19:27:09 +0530

    Frontend Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-text bg-'>
+      This is the start of hotel management system
+    </div>
+  )
+}
+
+export default App
